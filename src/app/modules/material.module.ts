f74c0b7a560495b46c44c08d68e04de3b5976a0d
Mohak Sharma <mohak2006@gmail.com>
2018-04-07 00:18:34 +0530

    Tabs Created

--- src/app/modules/material.module.ts
+++ src/app/modules/material.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material';
+import { MatTabsModule } from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-  imports: [MatButtonModule],
-  exports: [MatButtonModule],
+  imports: [
+    MatTabsModule,
+    MatIconModule
+  ],
+  exports: [
+    MatTabsModule,
+    MatIconModule
+  ],
   declarations: []
 })
 export class MaterialModule { }
